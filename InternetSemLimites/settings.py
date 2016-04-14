dc5dfb8ac8aaeda6368015b16714f890a0c9eaa6
Eduardo Cuducos <cuducos@gmail.com>
2016-04-14 14:36:27 -0300

    Typo

--- InternetSemLimites/settings.py
+++ InternetSemLimites/settings.py
@@ -109,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'pt-br`'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'America/Sao_Paulo'
 
