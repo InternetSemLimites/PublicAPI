646967d9c52469cfb169dc75926d1d47c5cd88c8
Eduardo Cuducos <cuducos@gmail.com>
2016-05-04 09:39:56 -0300

    Force utf-8 when reading fixtures

--- InternetSemLimites/core/migrations/0002_states_fixtures.py
+++ InternetSemLimites/core/migrations/0002_states_fixtures.py
@@ -8,7 +8,7 @@ def create_states(apps, schema_editor):
     State = apps.get_model('core', 'State')
     fixture_file = ('InternetSemLimites', 'core', 'fixtures', 'states.csv')
     fixture_path = path.join(settings.BASE_DIR, *fixture_file)
-    with open(fixture_path) as fh:
+    with open(fixture_path, encoding='utf-8') as fh:
         for line in reader(fh):
             State.objects.create(name=line[1], abbr=line[0])
 
