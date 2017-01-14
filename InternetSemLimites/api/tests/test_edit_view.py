41967e5ff9e46c192b8503e87a35e88b3022387a
Jonatas Baldin <jonatas.baldin@gmail.com>
2017-01-14 20:46:32 -0200

    Add tests for /api/provider/:id/edit/ endpoint

---
+++ InternetSemLimites/api/tests/test_edit_view.py
@@ -0,0 +1,98 @@
+from django.contrib.auth.models import User
+from django.core import mail
+from django.shortcuts import resolve_url
+from django.test import TestCase
+from InternetSemLimites.core.forms import ProviderForm
+from InternetSemLimites.core.models import Provider, State
+
+
+class TestGet(TestCase):
+
+    def setUp(self):
+        self.resp = self.client.get(resolve_url('api:edit', 1))
+
+    def test_get(self):
+        self.assertEqual(405, self.resp.status_code)
+
+
+class TestPostValid(TestCase):
+
+    def setUp(self):
+        User.objects.create_superuser(username='two', password='', email='42@xp.to')
+        sc, *_ = State.objects.get_or_create(abbr='SC', name='Santa Catarina')
+        go, *_ = State.objects.get_or_create(abbr='GO', name='Goiás')
+
+        self.provider = Provider.objects.create(
+            name='Xpto',
+            url='http://xp.to',
+            source='http://twitter.com/xpto',
+            category=Provider.SHAME,
+            other='Lorem ipsum'
+        )
+        self.provider.coverage.add(sc)
+        self.provider.coverage.add(go)
+
+        self.data = {
+            'name': 'XptoEdited',
+            'url': 'http://xpedited.to',
+            'source': 'http://twitter.com/xptoedited',
+            'coverage': [sc.pk],
+            'category': Provider.FAME,
+            'other': 'Lorem ipsum dolor'
+        }
+        self.resp = self.client.post(resolve_url('api:edit', self.provider.pk), self.data)
+
+    def test_post(self):
+        self.assertRedirects(self.resp, resolve_url('api:provider', self.provider.pk))
+
+    def test_send_email(self):
+        self.assertEqual(1, len(mail.outbox))
+
+    def test_edit(self):
+        edited_provider = Provider.objects.last()
+
+        self.assertEqual(edited_provider.name, self.data['name'])
+        self.assertEqual(edited_provider.url, self.data['url'])
+        self.assertEqual(edited_provider.source, self.data['source'])
+        self.assertEqual(edited_provider.category, self.data['category'])
+        self.assertEqual(edited_provider.other, self.data['other'])
+        self.assertEqual(edited_provider.coverage_id_to_list, self.data['coverage'])
+
+
+class TestPostInvalid(TestCase):
+
+    def setUp(self):
+
+        User.objects.create_superuser(username='two', password='', email='42@xp.to')
+        sc, *_ = State.objects.get_or_create(abbr='SC', name='Santa Catarina')
+        go, *_ = State.objects.get_or_create(abbr='GO', name='Goiás')
+
+        self.provider = Provider.objects.create(
+            name='Xpto',
+            url='http://xp.to',
+            source='http://twitter.com/xpto',
+            category=Provider.SHAME,
+            other='Lorem ipsum'
+        )
+        self.provider.coverage.add(sc)
+        self.provider.coverage.add(go)
+
+        self.resp = self.client.post(resolve_url('api:edit', self.provider.pk), dict())
+
+    def test_post(self):
+        self.assertEqual(200, self.resp.status_code)
+
+    def test_has_errors_on_empty_form(self):
+        json_resp = self.resp.json()
+        self.assertTrue(json_resp['errors'])
+
+    def test_has_errors_on_non_empty_form(self):
+        invalid_data = {'name': 'Xpto', 'coverage': ['xp', 'to'], 'url': ''}
+        resp = self.client.post(resolve_url('api:edit', self.provider.pk), invalid_data)
+        json_resp = resp.json()
+        errors = json_resp['errors']
+        with self.subTest():
+            self.assertEqual('Este campo é obrigatório.', errors['category'][0])
+            self.assertEqual('Este campo é obrigatório.', errors['source'][0])
+            self.assertEqual('Este campo é obrigatório.', errors['url'][0])
+            self.assertIn('não é um valor válido para', errors['coverage'][0])
