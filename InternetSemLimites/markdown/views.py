eee54eb464e72a48c059c2e6a10ac5a4104638a1
Eduardo Cuducos <cuducos@gmail.com>
2017-01-18 09:09:22 -0200

    Cleanup

--- InternetSemLimites/markdown/views.py
+++ InternetSemLimites/markdown/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from InternetSemLimites.core.models import Provider, State
 
 
--- InternetSemLimites/settings.py
+++ InternetSemLimites/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 import os
 from decouple import config, Csv
 from dj_database_url import parse as dburl
-from InternetSemLimites.core.apps import CoreConfig
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
