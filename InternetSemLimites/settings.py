a2bba053ed73e2c828ee50dbc161324d4d731285
Eduardo Cuducos <cuducos@gmail.com>
2018-01-25 11:59:41 -0200

    Move CORS middleware upper

--- InternetSemLimites/settings.py
+++ InternetSemLimites/settings.py
@@ -48,9 +48,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
